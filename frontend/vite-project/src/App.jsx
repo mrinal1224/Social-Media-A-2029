b408d112b1a2a6ebbefc5b3f42fa5202f7f196c4
mrinal1224 <iammrinalkc@gmail.com>
2026-08-31 10:35:49 +0530

    push

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
+import Login from './pages/Login.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import Landing from './pages/Landing'
+
 
 function App() {
 
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          {/* <Route path='/home' element={ <Login/>}/> */}
+
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
