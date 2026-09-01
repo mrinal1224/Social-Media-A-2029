c880fb1cbd57834aa16dbabaa3771f25fba2a47e
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-01 11:54:54 +0530

    fix protected route hook order

--- frontend/vite-project/src/components/ProtectedRoute.jsx
+++ frontend/vite-project/src/components/ProtectedRoute.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
-
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 
+function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth()
 
-
-function ProtectedRoute({children}) {
-   const {user , loading} = useAuth()
-
-   if(loading){
+  if (loading) {
     return <h1>Loading...</h1>
-   }
+  }
 
-   if(!user){
-     return Navigate('/login')
-   }
-
+  if (!user) {
+    return <Navigate to="/login" replace />
+  }
 
   return children
 }
