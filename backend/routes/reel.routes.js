19fb845dfd615b85d74f83df539364cb16bd0983
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-22 10:29:22 +0530

    feat: expose reel feed route

--- backend/routes/reel.routes.js
+++ backend/routes/reel.routes.js
@@ -2,12 +2,16 @@ import express from 'express'
 import isAuthenticated from '../middlewares/authMiddleware.js';
 
 import reelUpload from '../middlewares/reelUpload.middleware.js';
-import { createReel } from '../controllers/reel.controllers.js';
+import { createReel, getReels } from '../controllers/reel.controllers.js';
 
 
 const reelRoutes = express.Router();
 
 reelRoutes.post('/createReel', isAuthenticated, reelUpload.single('video'), createReel)
+
+// GET /reel
+// Home page uses this endpoint to load the latest reels.
+reelRoutes.get('/', isAuthenticated, getReels)
 
 
 
