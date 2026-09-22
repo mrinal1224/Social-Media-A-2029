8d6ca5cb23311a720eca5bb05de1ed207d369f2b
mrinal1224 <iammrinalkc@gmail.com>
2026-09-22 12:05:51 +0530

    Push Comment Model

--- backend/controllers/comment.controllers.js
+++ backend/controllers/comment.controllers.js
@@ -2,34 +2,50 @@ import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 
 
-export const createComment = (req , res)=>{
-  try {
+export const createComment = async (req, res) => {
+    try {
 
-       const {text} = req.body
-    
-      const post = Post.findById(req.params.id)
+        const { text } = req.body
 
-      const userId = req.user._id
+        // const post = Post.findById(req.params.id)
 
-      // next Steps
+        // const userId = req.user._id
 
-      if(!text){
-        res.status(400).json({ message: 'Comment Cannot be Empty ' })
-      }
+        // next Steps
 
-
-      if(text.length>500){
-        res.status(400).json({ message: 'Comment Cannot be more than 500 characters ' })
-      }
-
-      // Figure this out 
-
-      
+        if (!text) {
+            res.status(400).json({ message: 'Comment Cannot be Empty ' })
+        }
 
 
+        if (text.length > 500) {
+            res.status(400).json({ message: 'Comment Cannot be more than 500 characters ' })
+        }
+
+        // Figure this out 
+
+        await Comment.create({
+            text,
+            user: req.user._id,
+            post: req.params.id,
+
+        })
 
 
-  } catch (error) {
-    
-  }
+        res.status(201).json({ message: 'Comment Added' })
+
+
+
+
+
+
+
+
+    } catch (error) {
+        return res.status(500).json({ message: "Internal Server Error", error: error });
+    }
 }
+
+// get Comments
+
+// Delete Comments
